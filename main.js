34d5571385a5e3014ac0fb0948610b4c70c5e17f
hadicodess <85850834+hadicodess@users.noreply.github.com>
2021-09-23 22:06:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup() 
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500); 
+
+    canvas = createCanvas(550, 500); 
+    canvas.position(550, 500); 
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses); 
+} 
+function draw()
+{
+    background('#2CE863');
+} 
+function modelLoaded()
+{
+    console.log("PoseNet is initialized!");
+} 
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+    console.log(results);
+}
+}
